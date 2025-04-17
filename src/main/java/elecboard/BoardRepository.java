2b892c535524a031d9397a22f973297c8c071f7d
kunwoo0512 <dkzkcmzl39@khu.ac.kr>
2025-04-17 20:36:01 +0900

    modify controller, serveice, repository

--- src/main/java/elecboard/BoardRepository.java
+++ src/main/java/elecboard/BoardRepository.java
@@ -4,4 +4,6 @@ import elecboard.DTO.PageDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BoardRepository extends MongoRepository<PageDocument,String> {
+
+    PageDocument findByRoomId(String roomId);
 }
--- src/main/java/elecboard/BoardService.java
+++ src/main/java/elecboard/BoardService.java
@@ -1,30 +1,38 @@
 package elecboard;
 
+import elecboard.DTO.Sub.Payload;
+import elecboard.DTO.WhiteboardObjects.ImageObject;
+import elecboard.DTO.WhiteboardObjects.WhiteboardObject;
 import lombok.RequiredArgsConstructor;
 import elecboard.DTO.Page;
 import elecboard.DTO.PageDocument;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
 
-    public PageDocument savePage(Page page){
+    public PageDocument saveOrUpdatePage(Page page) {
+        PageDocument existing = boardRepository.findByRoomId(page.getRoomId());
 
-        PageDocument doc = new PageDocument();
-        doc.setUserNames(page.getUserNames());
+        //기존 roomId가 존재하면 업데이트, 아니면 새롭게 저장
+        PageDocument doc;
+        if(existing != null){
+            doc = existing;
+            //이미지 url이 바뀌었는지도 check해야 함
+
+        } else{
+            doc = new PageDocument();
+        }
         doc.setRoomId(page.getRoomId());
         doc.setRoomName(page.getRoomName());
+        doc.setUserNames(page.getUserNames());
         doc.setObjects(page.getObjects());
 
-        return boardRepository.save(doc);
-    }
-
-    //id가 없는 값을 경우 null이므로 Optional
-    public Optional<PageDocument> findPageById(String id) {
-        return boardRepository.findById(id);
+        return boardRepository.save(doc); // 저장 or 업데이트
     }
 }
