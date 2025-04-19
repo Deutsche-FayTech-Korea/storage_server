a9220870463e934fb9fa1b08487670a020451586
kunwoo0512 <dkzkcmzl39@khu.ac.kr>
2025-04-19 12:13:01 +0900

    add dashboard

--- src/main/java/elecboard/BoardController.java
+++ src/main/java/elecboard/BoardController.java
@@ -1,12 +1,14 @@
 package elecboard;
 
+import elecboard.DTO.Dashboard;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import elecboard.DTO.Page;
 import elecboard.DTO.PageDocument;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,15 +17,15 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
     private final BoardService boardService;
 
-//    @PostMapping("/save/{roomId}")
-//    public ResponseEntity<String> savePage(@RequestBody Page page){
-//        boardService.savePage(page);
-//        return ResponseEntity.ok("Saved successfully");
-//    }
-
     @PostMapping("/save")
     public ResponseEntity<String> savePage(@RequestBody Page page) {
         PageDocument saved = boardService.saveOrUpdatePage(page);
         return ResponseEntity.ok("Saved successfully");
+    }
+
+    @GetMapping("/page/{userName}")
+    public ResponseEntity<List<Dashboard>> getBoardsByUser(@PathVariable String userName) {
+        List<Dashboard> result = boardService.getBoardsByUser(userName);
+        return ResponseEntity.ok(result);
     }
 }
