afd570c23d50d6d97525d2adc12fb128831fe9b0
kunwoo0512 <dkzkcmzl39@khu.ac.kr>
2025-05-18 17:27:55 +0900

    add auth

--- src/main/java/elecboard/DTO/PageDocument.java
+++ src/main/java/elecboard/DTO/PageDocument.java
@@ -1,5 +1,6 @@
 package elecboard.DTO;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Id;
 import lombok.Data;
 import elecboard.DTO.WhiteboardObjects.WhiteboardObject;
@@ -9,12 +10,14 @@ import java.util.List;
 
 //db에 넣을 때 꺼낼 때 사용, 외부와 통신할 때는 직접적으로 사용x(Page에 담아서 전달)
 @Document(collection = "whiteboard_pages")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class PageDocument {
     @Id
     private String id;
 
     private List<String> userNames;
+    private String createdBy;
     private String roomId;
     private String roomName;
     private List<WhiteboardObject> objects;
--- src/main/java/elecboard/DTO/WhiteboardObjects/WhiteboardObject.java
+++ src/main/java/elecboard/DTO/WhiteboardObjects/WhiteboardObject.java
@@ -17,7 +17,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
         @JsonSubTypes.Type(value = ImageObject.class, name = "image"),
 })
 public abstract class WhiteboardObject {
-    private String id;
     //private String objectType; -> 12번 줄에서 정의해줌
     private String createdBy;
 }
